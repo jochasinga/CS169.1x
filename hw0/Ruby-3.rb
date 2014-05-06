f4bbaffe1f46de2241fdad627c106a77d128d882
Joe Chasinga <jo.chasinga@gmail.com>
2014-05-07 00:54:19 +0700

    Fix Ruby-1.rb

--- hw0/Ruby-3.rb
+++ hw0/Ruby-3.rb
@@ -5,19 +5,19 @@ class BookInStock
    attr_accessor :isbn, :price
    
    def initialize(isbn, price)
-    raise TypeError unless price.is_a? Float and isbn.is_a? String
+       raise TypeError unless price.is_a? Float and isbn.is_a? String
       
-    @isbn=isbn
-    @price=price
+       @isbn=isbn
+       @price=price
     
-      if @price <= 0 || @isbn == ''
-        raise ArgumentError, "@price <= 0 or @isbn == ''", caller
-      end
+       if @price <= 0 || @isbn == ''
+           raise ArgumentError, "@price <= 0 or @isbn == ''", caller
+       end
    end
   
-  # Convert @price to string currency format
+   # Convert @price to string currency format
    def price_as_string()
-    return "$#{@price}0" 
+       return "$#{@price}0" 
    end
    
 end
