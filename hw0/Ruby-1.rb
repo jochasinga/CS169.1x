883789c1daad859a4a6d97d3add5e5bb66628570
Joe Chasinga <jo.chasinga@gmail.com>
2014-05-08 01:02:52 +0700

    Final fix hw0 part 1

--- hw0/Ruby-1.rb
+++ hw0/Ruby-1.rb
@@ -1,32 +1,30 @@
-#!/usr/bin/env ruby
+##!/usr/bin/env ruby
 
 def sum(int_array)
   int_array.empty? ? 0 : int_array.inject(:+)
 end
 
 def max_2_sum(int_array)
-  case 
-  when int_array.empty?
-       0
-  when int_array.length==1
-       int_array[0]
+
+  if int_array.empty?
+     0
+  elsif int_array.length == 1
+     int_array[0]
   else
-       int_array.sort.last(2).inject(:+)
+     int_array.sort.last(2).inject(:+)
   end
 end
 
-def sum_2_n?(int_array, n)
+def sum_to_n?(int_array, n)
 
-    limit = int_array.length
-
-    if limit<= 1
-       false
+    if int_array.length > 1
+       c = int_array.combination(2).to_a.map {|i| i.inject(:+)}.include?(n)
     else
-       (0..limit).each do |i|
-           ((i+1)...limit).each do |j|
-	       break if int_array[i]+int_array[j]==n
-           end
-           return true
-       end
+       false
     end
 end
+
+puts sum_to_n?([1,2,3,4,5], 5).to_s + " should == true"
+puts sum_to_n?([-1,-2,3,4,5,-8], 12).to_s + " should == false"
+puts sum_to_n?([1], 1).to_s + " should == false"
+puts sum_to_n?([], 0).to_s + " should == false"
